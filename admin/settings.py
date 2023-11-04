2254acfe10aafd3d41bed36794643bbc414b75cc
ashvn24 <ashwinashi6@gmail.com>
2023-11-04 20:42:55 +0530

    port

--- admin/settings.py
+++ admin/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         "USER": "postgres",
         "PASSWORD": "0089ashi",
         "HOST": "localhost",
-        "PORT": "5432",
+        "PORT": "5433",
     }
 }
 # DATABASES = {
